dfd68343b70e0461bc4504272dc5ebf8a90e6ebd
Rouge <130969125+RougeLi@users.noreply.github.com>
2025-08-03 10:51:13 +0800

    Add ReplaceDateWithString utility type for recursive type transformation (#19)
    Introduced a new utility type `ReplaceDateWithString` to recursively replace all `Date` objects with `string` types within a given type `T`.

--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -7,3 +7,19 @@ export * from './module.types';
 export * from './prisma.types';
 export * from './logger.types';
 export * from './startup-module.types';
+
+/**
+ * Recursively replaces all Date objects
+ * with a string type in a given type T
+ * */
+export type ReplaceDateWithString<T> = T extends Date
+  ? string
+  : T extends Function
+    ? T
+    : T extends any[]
+      ? number extends T['length']
+        ? ReplaceDateWithString<T[number]>[]
+        : { [K in keyof T]: ReplaceDateWithString<T[K]> }
+      : T extends object
+        ? { [K in keyof T]: ReplaceDateWithString<T[K]> }
+        : T;
